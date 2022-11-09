fc984803e38ab996b4b763ed74601a5a10f530f4
Karter298 <pomazanov.roma@yandex.ru>
2022-11-09 13:58:18 +0300

    add: bg

--- footer.php
+++ footer.php
@@ -172,7 +172,7 @@
         <div class="footer-wrapper">
           <div class="footer-legal">
             <p class="footer-copyright">
-              &copy; <?php echo date("Y")?> «Aliance Production». Все права защищены.
+              &copy; <?= date("Y")?> «Aliance Production». Все права защищены.
             </p>
             <a href="#" class="footer-policy">Политики конфиденциальности</a>
           </div>

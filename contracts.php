1b23dba8e8d0117ece172eab416aef472b6ceba1
Karter298 <pomazanov.roma@yandex.ru>
2022-11-11 19:09:35 +0300

    add: contracts page + PP

--- contracts.php
+++ contracts.php
@@ -1,8 +1,27 @@
 <?php 
 $page_title = 'Контрактное производство';
-$header_style = "page-header-img";
-$breadcrumb_style = "breadcrumb-item-link-white";
+$header_style = "page-bg-grey";
+$breadcrumb_style = "breadcrumb-item-link-gr";
 include_once('header-page.php');
-include_once('./template-parts/contracts-block.php');
 ?>
+<div class="contracts-him">
+<?php include_once('./template-parts/contracts-block.php');?>
+</div>
+<section class="section seo">
+<div class="container container-seo">
+  <h3 class="title-seo">
+    SEO Заголовок
+  </h3>
+  <p class="text-seo">
+    Также как дальнейшее развитие различных форм деятельности предполагает независимые способы реализации существующих финансовых и административных условий. Каждый из нас понимает очевидную вещь: курс на социально-ориентированный национальный проект играет важную роль в формировании как самодостаточных, так и внешне зависимых концептуальных решений. Внезапно, интерактивные прототипы набирают популярность среди определенных слоев населения, а значит, должны быть...
+  </p>
+  <div class="link-seo">
+    <svg width="24" height="24" class="seo-icon">
+      <use href="img/sprite.svg#more"></use>
+    </svg>
+    <a href="#" class="seo-readmore">Читать больше</a>
+  </div>
+</div>
+</section>
+
 <?php include_once('footer.php');?>
